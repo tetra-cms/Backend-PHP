4759d3ed9e76fb5fe61375cf55a87469a7c79076
fet1sov <august9222@gmail.com>
2026-07-17 18:26:54 +0500

    feat(spa): добавление SPA маршрутов

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/admin/{any?}', function () {
+    return response()->file(public_path('admin/index.html'));
+})->where('any', '.*');
+
+Route::get('/{any?}', function () {
+    return response()->file(public_path('app/index.html'));
+})->where('any', '^(?!api|admin).*$');
